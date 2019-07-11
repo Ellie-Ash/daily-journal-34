b3c4ebeec36603539581d7470435f48769e96739
Ellie Ash <ellieash4000@gmail.com>
2019-07-11 15:07:55 -0500

    accidentally worked on master

---
+++ main.js
@@ -0,0 +1,14 @@
+const journalEntry = {
+    date: "06-11-2019",
+    covered: "HTML/CSS",
+    entry: "Today we reviewed some basics",
+    mood: "happy"
+}
+
+
+const allEntries = [];
+
+allEntries.push(journalEntry);
+
+console.log(allEntries)
+
