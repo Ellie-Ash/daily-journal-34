0aae04ebff2063849ae837706c34b11c3f4f08b5
Ellie Ash <ellieash4000@gmail.com>
2019-07-11 15:19:08 -0500

    whats goin on

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-const journalEntry = {
-    date: "06-11-2019",
+console.log("hello")
+
+const journalEntry1 = {
+    date: "07-11-2019",
     covered: "HTML/CSS",
     entry: "Today we reviewed some basics",
     mood: "happy"
+    
 }
 
+const journalEntry2 = {
+    date: "07-12-2019",
+    covered: "Javascript",
+    entry: "I practiced using JS objects and arrays",
+    mood: "neutral"
+}
+
+const journalEntry3 = {
+    date: "07-13-2019",
+    covered: "Career",
+    entry: "We listened to a lecture about LinkedIn",
+    mood: "neutral"
+}
 
 const allEntries = [];
 
 allEntries.push(journalEntry);
 
 console.log(allEntries)
+
+
 
