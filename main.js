b3e0d08bfb7d5f1a10abc76ba7140e9d94ada399
Ellie Ash <ellieash4000@gmail.com>
2019-07-22 10:06:27 -0500

    idk

--- main.js
+++ main.js
@@ -1,35 +1,4 @@
-const journalEntries = [
-    {
-        date: "07-01-19",
-        topic: "HTML/CSS",
-        entry: "We styled things with CSS by accessing class/id from HTML",
-        mood: "happy"
-    },
-    {
-        date: "07-02-19",
-        topic: "Javascript",
-        entry: "We learned how to manipulate the DOM with query selectors",
-        mood: "neutral"
-    },
-    {
-        date: "07-03-19",
-        topic: "Javascript",
-        entry: "Today we learned how to use array methods",
-        mood: "neutral"
-    },
-    {
-        date: "07-04-19",
-        topic: "Javascript",
-        entry: "We learned the difference between arrays and objects and when/why we should use them",
-        mood: "happy"
-    },
-    {
-        date: "07-05-19",
-        topic: "Other",
-        entry: "Today we learned about GitHub and how to use it in a group project",
-        mood: "frustrated"
-    },
-]
+
 
 
 // Then store a reference to an existing HTML element
